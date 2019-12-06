eaa89f380efd2fd069267f0f30ea5f0d6153bbde
Anton Sobinov <an.sobinov@gmail.com>
2019-12-06 17:11:37 -0600

    Change License

--- setup.py
+++ setup.py
@@ -4,10 +4,6 @@
 NCams Toolbox
 Copyright 2019 Charles M Greenspon, Anton Sobinov
 https://github.com/CMGreenspon/NCams
-
-Please see AUTHORS for contributors.
-https://github.com/CMGreenspon/NCams/blob/master/README.md
-Licensed under the Apache License, Version 2.0
 """
 
 from setuptools import setup, find_packages
@@ -20,7 +16,6 @@ setup(
     long_description_content_type="text/markdown",
     url='https://github.com/CMGreenspon/NCams',
     install_requires=[
-        'deeplabcut', # Do we need it to include deeplabcut? They might be using a different package for tracking
         'moviepy',
         'opencv-contrib-python',
         'reportlab',
